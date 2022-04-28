0ce711d386acae98c498f58f9a56909283516a64
NayeemNishaat <rxnayeemimi@gmail.com>
2022-04-28 20:30:28 +0600

    implemented cache with redis

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const Redis = require("redis");
+// const { createClient } = require("redis");
+
+// Point: Start Srver
+// sudo service redis-server start
+const redisClient = Redis.createClient(); // Note: Pass URL for the production!
+const DEFAULT_EXPIRATION = 3600;
+(async () => {
+	await redisClient.connect();
+	redisClient.on("error", (err) => console.log("Redis Client Error", err));
+})();
 
 const app = express();
 app.use(cors());
 
 app.get("/photoes", async (req, res) => {
-	const albumId = req.query.albumId;
+	const photoes = await redisClient.get("photoes");
 
-	const result = await fetch(`https://jsonplaceholder.typicode.com/photos`);
+	if (photoes !== null) {
+		res.status(200).json(JSON.parse(photoes));
+	} else {
+		// Point: Cache
+		const result = await fetch(
+			`https://jsonplaceholder.typicode.com/photos`
+		);
 
-	const data = await result.json();
+		const data = await result.json();
 
-	res.status(200).json(data);
+		await redisClient.setEx(
+			"photoes",
+			DEFAULT_EXPIRATION,
+			JSON.stringify(data)
+		);
+
+		res.status(200).json(data);
+	}
 });
 
 app.get("/photos:id", async (req, res) => {
@@ -25,3 +49,6 @@ app.get("/photos:id", async (req, res) => {
 });
 
 app.listen(3001);
+
+// flushall // Note: Clear redis cache
+// keys * // Note: Get all keys
